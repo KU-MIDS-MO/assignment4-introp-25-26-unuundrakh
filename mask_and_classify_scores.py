b981e45f7f0992743f670af006abd9e606beb967
Unu-Undrakh Zorigtbaatar <unu-undrakhzorigtbaatar@Unu-Undrakhs-MacBook-Air-2.local>
2025-12-03 12:46:05 +0100

    assignment is done

--- mask_and_classify_scores.py
+++ mask_and_classify_scores.py
@@ -1,10 +1,34 @@
 import numpy as np
 
 def mask_and_classify_scores(arr):
-    """
-    write your solution here;
-    follow the instructions in the README.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+   if not isinstance(arr, np.ndarray) or arr.ndim != 2 or arr.shape[0] != arr.shape[1] or arr.shape[0] < 4:
+       return None
+   cleaned = arr.copy()
+    
+   cleaned[cleaned < 0] = 0
+   cleaned[cleaned > 100] = 100
+                 
+   levels = np.zeros(cleaned.shape, dtype=int)
+                       
+   for i in range(cleaned.shape[0]):
+        for j in range(cleaned.shape[1]):
+            value = cleaned[i, j]
+            if value < 40:
+                levels [i, j]= 0
+            elif 40 <= value < 70:
+                levels[i, j]= 1
+            else:
+                levels[i, j]= 2
+    
+   n = arr.shape[0]
+   row_pass_counts = np.zeros(n, dtype=int)
+    
+   for i in range(cleaned.shape[0]):
+         count = 0
+         for value in cleaned[i]: 
+             if value >= 50:
+                 count += 1  
+                 row_pass_counts[i] = count
+    
+   return cleaned, levels, row_pass_counts
+        
